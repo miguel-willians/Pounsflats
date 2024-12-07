6940a0657484fb246150d71a327f935c9c299e4b
miguel-willians <miiguel.wso@gmail.com>
2024-12-07 14:37:29 -0300

    Criando o CheckInput

--- src/components/CheckInput.jsx
+++ src/components/CheckInput.jsx
@@ -2,17 +2,26 @@
 
 import styles from "./CheckInput.module.css";
 
+import StarIcon from "../assets/icons/StarIcon.svg";
+
 function CheckInput({ checked, title, location, avgReview, perDay }) {
   return (
     <div className={styles.inpBox}>
-      <input type="checkbox" defaultChecked={checked} />
+      <input type="checkbox" defaultChecked={checked} id={styles.checkbox} />
       <div>
-        <h3>{title}</h3>
-        <p>{location}</p>
-      </div>
-      <div>
-        <span>{avgReview}</span>
-        <p>Diária: {perDay}</p>
+        <div>
+          <div>
+            <h3>{title}</h3>
+            <div>
+              <img src={StarIcon} alt="Star Icon" />
+              {avgReview}
+            </div>
+          </div>
+          <div>
+            <p>{location}</p>
+            <p>Diária: {perDay}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
