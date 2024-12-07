1d22a58004b865b06cc89c3e61efede2bfebdd38
miguel-willians <miiguel.wso@gmail.com>
2024-12-07 17:06:50 -0300

    Adicionando interatividade em Message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
 import styles from "./Message.module.css";
-
 import CloseIcon from "../assets/icons/CloseIcon.svg";
 
 function Message({ title, desc }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div className={styles.messageBox}>
       <div>
         <p>{title}</p>
-        <img src={CloseIcon} alt="Close Icon" />
+        <img src={CloseIcon} alt="Close Icon" onClick={handleClose} />
       </div>
       <p>{desc}</p>
     </div>
